dc792d956bdb5f8828b2ab85f4569417556ac915
Juan <juankrlos1219@hotmail.com>
2024-05-23 10:25:42 -0500

    favoritos funcionando

--- server.mjs
+++ server.mjs
@@ -25,13 +25,12 @@ async function createConnection() {
     connection = await mysql.createConnection(dbConfig);
   } catch (error) {
     console.error('Error conectando a la base de datos:', error);
-    process.exit(1); // Salir del proceso con código de error
+    process.exit(1);
   }
 }
 
 createConnection();
 
-// Middleware para gestionar el token de sesión
 app.use((req, res, next) => {
   let token = req.cookies.session_token;
   if (!token) {
@@ -47,6 +46,15 @@ app.post('/api/favorito', async (req, res) => {
   const session_token = req.session_token;
 
   try {
+    const [rows] = await connection.execute(
+      'SELECT * FROM favorito WHERE session_token = ? AND libro_id = ? AND capitulo = ? AND versiculo = ?',
+      [session_token, libro_id, capitulo, versiculo]
+    );
+    
+    if (rows.length > 0) {
+      return res.status(400).json({ error: 'El versículo ya está en favoritos' });
+    }
+
     const [result] = await connection.execute(
       'INSERT INTO favorito (session_token, libro_id, capitulo, versiculo) VALUES (?, ?, ?, ?)',
       [session_token, libro_id, capitulo, versiculo]
@@ -57,6 +65,32 @@ app.post('/api/favorito', async (req, res) => {
     res.status(500).json({ error: 'Error Interno del Servidor' });
   }
 });
+
+// Endpoint para eliminar un favorito específico por ID de libro
+app.delete('/api/favorito/libro/:libro_id', async (req, res) => {
+  console.log('DELETE request received at /api/favorito/libro/:libro_id');
+  const libroId = req.params.libro_id;
+  const session_token = req.session_token;
+  
+  console.log('ID del libro del favorito a eliminar:', libroId);
+  
+  try {
+    const [result] = await connection.execute(
+      'DELETE FROM favorito WHERE libro_id = ? AND session_token = ?',
+      [libroId, session_token]
+    );
+
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: 'Favoritos del libro eliminados exitosamente' });
+    } else {
+      res.status(404).json({ error: 'Favoritos del libro no encontrados' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error Interno del Servidor' });
+  }
+});
+
 
 app.get('/api/favorito', async (req, res) => {
   const session_token = req.session_token;
@@ -77,6 +111,25 @@ app.get('/api/libro', async (req, res) => {
   try {
     const [rows] = await connection.execute('SELECT * FROM libro');
     res.json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error Interno del Servidor' });
+  }
+});
+
+app.get('/api/versiculo/:libro_id/capitulo/:capitulo/versiculo/:versiculo', async (req, res) => {
+  const { libro_id, capitulo, versiculo } = req.params;
+
+  try {
+    const [rows] = await connection.execute(
+      'SELECT * FROM versiculo WHERE libro_id = ? AND capitulo = ? AND versiculo = ?',
+      [libro_id, capitulo, versiculo]
+    );
+    if (rows.length > 0) {
+      res.json(rows);
+    } else {
+      res.status(404).json({ error: 'Versículo no encontrado' });
+    }
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Error Interno del Servidor' });
